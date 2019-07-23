ab162f483f41c16cf4e95e81020d39ef9511a17e
Hanliu <49193443+hanAndHan@users.noreply.github.com>
2019-07-23 16:41:11 +0800

    Update Buffer.h

--- codes/TCP/Buffer.h
+++ codes/TCP/Buffer.h
@@ -1,10 +1,12 @@
-﻿// @Author Liu Han
+// @Author Liu Han
 // @Email  hancurrent@foxmail.com
 #pragma once
 #include <algorithm>
 #include <vector>
 #include <assert.h>
 #include <string.h>
+//        kCheapPrepend                           readerIndex_            writerIndex_
+//   |         |	prependableBytes()-kCheapPrepend 	|   readableBytes()   |   writableBytes()  |
 
 class Buffer
 {
@@ -12,7 +14,7 @@ public:
 
 	static const size_t kCheapPrepend = 8;
 	static const size_t kInitialSize = 1024;
-
+	//头部预留了8字节以备不时之需
 	explicit Buffer(size_t iniSize = kInitialSize)
 		:buffer_(kCheapPrepend + iniSize),
 		readerIndex_(kCheapPrepend),
@@ -20,36 +22,25 @@ public:
 	{
 	}
 
-	void swap(Buffer& tmp)
-	{
-		buffer_.swap(tmp.buffer_);
-		std::swap(readerIndex_, tmp.readerIndex_);
-		std::swap(writerIndex_, tmp.writerIndex_);
-	}
-
-	size_t readableBytes() const   //可读的数据量
+	size_t readableBytes() const   //可读空间大小
 	{
 		return writerIndex_ - readerIndex_;
 	}
-	size_t writableBytes() const   //可写的空间
+	size_t writableBytes() const   //可写空间大小
 	{
 		return buffer_.size() - writerIndex_;
 	}
-	size_t prependableBytes() const  //前面空闲空间
+	size_t prependableBytes() const  //readerIndex前面空间的大小
 	{
 		return readerIndex_;
 	}
 
-	void prepend(const void* data, size_t len)   //在头部添加
+	void prepend(const void* data, size_t len)   //在/readerIndex前面添加
 	{
 		assert(len <= prependableBytes());
 		readerIndex_ -= len;
 		const char* d = static_cast<const char*>(data);
 		std::copy(d, d + len, begin() + readerIndex_);
-	}
-	const char* peek() const         // 可读的位置下标
-	{
-		return begin() + readerIndex_;
 	}
 
 	const char* findCRLF() const     //查找换行符\r\n
@@ -57,35 +48,28 @@ public:
 		const char* crlf = std::search(peek(), beginWrite(), RN, RN + 2);
 		return crlf == beginWrite() ? NULL : crlf;
 	}
-	const char* findCRLF(const char* start) const
+	const char* findCRLF(const char* start) const  //从指定位置开始查找换行符\r\n
+
 	{
 		assert(peek() <= start);
 		assert(start <= beginWrite());
-		// FIXME: replace with memmem()?
 		const char* crlf = std::search(start, beginWrite(), RN, RN + 2);
 		return crlf == beginWrite() ? NULL : crlf;
 	}
 
-	const char* findEOL() const          //查找文件结尾符
-	{
-		const void* eol = memchr(peek(), '\n', readableBytes());
-		return static_cast<const char*>(eol);
-	}
-	const char* findEOL(const char* start) const
-	{
-		assert(peek() <= start);
-		assert(start <= beginWrite());
-		const void* eol = memchr(start, '\n', beginWrite() - start);
-		return static_cast<const char*>(eol);
-	}
-
-	void append(const char* data, size_t len)
+	void append(const char* data, size_t len)   //追加数据到缓冲
 	{
 		ensureWritableBytes(len);
 		std::copy(data, data + len, beginWrite());
 		hasWritten(len);
 	}
-	void ensureWritableBytes(size_t len)
+
+	void append(const void* data, size_t len)
+	{
+		append(static_cast<const char*>(data), len);
+	}
+
+	void ensureWritableBytes(size_t len)  //确认缓冲区大小是否大于等于len，长度不够则扩容
 	{
 		if (writableBytes() < len)
 		{
@@ -93,33 +77,34 @@ public:
 		}
 		assert(writableBytes() >= len);
 	}
-	void append(const void* data, size_t len)
+	
+	const char* peek() const         // 可读的位置的指针
 	{
-		append(static_cast<const char*>(data), len);
+		return begin() + readerIndex_;
 	}
 
-	char* beginWrite()                //开始可写的位置
-	{
-		return begin() + writerIndex_;
-	}
-
-	const char* beginWrite() const    //开始可写的位置
+	char* beginWrite()                //可写的位置的指针
 	{
 		return begin() + writerIndex_;
 	}
 
-	void hasWritten(size_t len)
+	const char* beginWrite() const    //可写的位置的指针
+	{
+		return begin() + writerIndex_;
+	}
+
+	void hasWritten(size_t len)      //可写部分减少
 	{
 		assert(len <= writableBytes());
 		writerIndex_ += len;
 	}
 
-	void unwrite(size_t len)       // 回写len长度字段
+	void unwrite(size_t len)        //可写部分增加
 	{
 		assert(len <= readableBytes());
 		writerIndex_ -= len;
 	}
-	void retrieve(size_t len) // 释放可读部分
+	void retrieve(size_t len)	   //释放已读部分
 	{
 		assert(len <= readableBytes());   
 		if (len < readableBytes())
@@ -142,7 +127,7 @@ public:
 		readerIndex_ = kCheapPrepend;
 		writerIndex_ = kCheapPrepend;
 	}
-	void retrieveUntil(const char* end) //释放直到end的数据
+	void retrieveUntil(const char* end) //释放直到end的可读数据
 	{
 		assert(peek() <= end);
 		assert(end <= beginWrite());
@@ -165,8 +150,11 @@ private:
 	}
 
 	void makeSpace(size_t len)
-	{
-		if (writableBytes() + prependableBytes() < len + kCheapPrepend)  //保留了8个字节的头部
+	{ 
+	//先看可写的部分够不够，不够才扩容
+		//         kCheapPrepend                       readerIndex_            writerIndex_
+		//  |         |	  prependableBytes()-kCheapPrepend 	|   readableBytes()   |   writableBytes()  |
+		if (writableBytes() + prependableBytes() - kCheapPrepend < len )  //保留了8个字节的头部
 		{
 			buffer_.resize(writerIndex_ + len);
 		}
@@ -174,9 +162,7 @@ private:
 		{
 			assert(kCheapPrepend < readerIndex_);
 			size_t readable = readableBytes();
-			std::copy(begin() + readerIndex_,
-				begin() + writerIndex_,
-				begin() + kCheapPrepend);
+			std::copy(begin() + readerIndex_,begin() + writerIndex_,begin() + kCheapPrepend);
 			readerIndex_ = kCheapPrepend;
 			writerIndex_ = readerIndex_ + readable;
 			assert(readable == readableBytes());
